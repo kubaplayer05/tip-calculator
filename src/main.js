7193f8ee1d3ee501770858b3ca3bcc581b3e6f35
cos <cos>
2022-11-08 07:58:38 +0100

    add input validation

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ tipBtns.forEach((tip) => {
 		selectedTip = e.target.attributes[1].value;
 		checkTipBtns();
 		e.target.classList.add("active");
-        calculateTip()
+		calculateTip();
 	});
 });
 
@@ -31,37 +31,52 @@ const checkTipBtns = () => {
 	});
 };
 
-const inputValidation = () => {
-    if(billInput <= 0) {
+const billValidation = () => {
+	if (billInput.value < 0) {
+		billInput.classList.add("error");
+		return false;
+	} else {
+		billInput.classList.remove("error");
+		return true;
+	}
+};
 
-    }
-}
+const peopleValidation = () => {
+	if (peopleInput.value <= 0) {
+		peopleInput.classList.add("error");
+		return false;
+	} else {
+		peopleInput.classList.remove("error");
+		return true;
+	}
+};
 
 const resetAllValues = () => {
-    customValueInput.value = ''
-    billInput.value = 0
-    peopleInput.value = 0
-}
+	customValueInput.value = "";
+	billInput.value = 0;
+	peopleInput.value = 1;
+	amount.textContent = `$ 0.00`;
+	total.textContent = `$ 0.00`;
+};
 
 const calculateTip = () => {
 	billValue = parseFloat(billInput.value);
 	peopleCount = peopleInput.value;
 
-	console.log(billValue);
-	console.log(selectedTip);
-	console.log(peopleCount);
+	if (peopleValidation() && billValidation()) {
+		const amountValue = (billValue * selectedTip) / peopleCount;
+		const totalValue = (billValue * selectedTip + billValue) / peopleCount;
 
-	const amountValue = (billValue * selectedTip) / peopleCount;
-	const totalValue = (billValue * selectedTip + billValue) / peopleCount;
-
-	amount.textContent = `$ ${amountValue.toFixed(2)}`;
-	total.textContent = `$ ${totalValue.toFixed(2)}`;
+		amount.textContent = `$ ${amountValue.toFixed(2)}`;
+		total.textContent = `$ ${totalValue.toFixed(2)}`;
+	}
 };
 
 billInput.addEventListener("input", calculateTip);
+customValueInput.addEventListener("click", checkTipBtns);
 customValueInput.addEventListener("input", () => {
 	selectedTip = customValueInput.value / 100;
 	calculateTip();
 });
 peopleInput.addEventListener("input", calculateTip);
-reset.addEventListener('click',resetAllValues)
+reset.addEventListener("click", resetAllValues);
